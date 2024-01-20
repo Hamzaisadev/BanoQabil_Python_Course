935fa637818e47d99af1644281c147546d965a6e
Hamzacoder12 <156201157+Hamzacoder12@users.noreply.github.com>
2024-01-21 03:48:38 +0500

    Update Hamza_ishaq_midterm.py

--- Hamza_ishaq_midterm.py
+++ Hamza_ishaq_midterm.py
@@ -3,40 +3,58 @@
 #Question Number 1
 #Write a Python program to do arithmetical operations addition and division.
 #1.1
+
 number1 = int(input("Enter Your First Number Here"))
 number2 = int(input("Enter Your Second Number Here"))
 Total = number1 + number2
+
 print ("The Sum of",number1,"+",number2,"is",Total)
+
 #1.2
+
 dividend = int(input("Enter Dividend For Division"))
 divisor = int(input("Enter Divisor For Division"))
 result = dividend / divisor
+
 print ("The Division of",dividend,"/",divisor,"is",result)
+
 #1.3
+
 num = int(input("Enter Number"))
 power = int(input("Enter Power"))
 result = num**power
+
 print("value of", num, "to power of", power, "is", result)
+
 #1.4
+
 Divident = int(input("Enter Divident"))
 Divider = int(input("Enter Divider"))
 result = Divident//Divider
+
 print("Floor Division Of", Divident, "//" ,Divider, "is", result)
+
 #Question number2
+
 #Write a Python program to find the area of a triangle.
 #The formula to the area of a triangle is b*h/2 as done in 30 line
+
 base = int(input("Enter the length of the base of the triangle"))
 height = int(input("Enter the height of the triangle"))
 area = base*height/2
 print ("the area of the triangle is",area)
+
 #Question number 3
 #Write a Python program to convert Celsius to Fahrenheit.
 #formula to convert Celsius into Fahrenheit is F = (C * 9/5) + 32
+
 Celsius = int(input("Enter temprature in Celsius"))
 result = Celsius*9/5+32
 print (Celsius,"degrees Celsius is equal to", result, "degrees Fahrenheit")
+
 #Question Number 4
 #Write a Python program that return all datatypes that we discussed in the class.
+
 a = 10
 b = 10.8
 c = "hello world"
