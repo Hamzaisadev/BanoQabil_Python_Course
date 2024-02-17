2d7fa3084f2b1b99790a18d56b50fb89cb7a31c7
HamzaIshaq <156201157+Hamzaisadev@users.noreply.github.com>
2024-02-18 03:06:44 +0500

    Update POS_SYSTEM.py

---
+++ POS SYSTEM/POS_SYSTEM.py
@@ -0,0 +1,19 @@
+from cProfile import label
+from sys import maxsize
+from tkinter import *
+
+
+
+root = Tk()
+
+
+
+root.geometry("480x480")
+root.minsize(500,500)
+root.maxsize(900,900)
+
+hamza = Label(text="I am a coder")
+hamza.tk_focusFollowsMouse
+hamza.pack()
+
+root.mainloop()
